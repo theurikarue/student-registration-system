495b632825d6ae527d17f20af4637cc9d4d6883c
Bonitheuri <133132495+theurikarue@users.noreply.github.com>
2023-07-26 00:35:32 -0700

    Update models.py
    modification in code

--- student_register/models.py
+++ student_register/models.py
@@ -1,16 +1,28 @@
 from django.db import models
 
 # Create your models here.
- 
-class Student( models.Model) :
+
+class Student(models.Model):
     Name = models.CharField(max_length=100)
-    Reg_no=models.CharField(max_length=50)
-    Mobile_no=models.IntegerField
-    Student_email=models.EmailField(max_length=20)
+    Reg_no = models.CharField(max_length=50)
+    Mobile_no = models.IntegerField(null=False)
+    Student_email = models.EmailField(max_length=20)
+    Intake_year = models.DateField(auto_now=False)
+
+class Hostel(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    Student_stay = models.BooleanField(default=False)
+    Members_in_room = models.IntegerField(null=False)
+
+    
+    
+
+ 
+    
+    
 
 
-class Hostel (models.Model):
-    Student_stay =models.BooleanField(default=False)
+
 
  
     
