fe0c1544f213454d95dd263fa4d2d2a239dc33ad
Bonitheuri <133132495+theurikarue@users.noreply.github.com>
2023-07-25 23:41:03 -0700

    Update models.py

--- school_fees/models.py
+++ school_fees/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Finance(models.Model):
-    Total_fee = models.IntegerField
-    Fee_balance = models.IntegerField
-    Fee_paid = models.IntegerField
+    Total_fee = models.IntegerField()
+    Fee_balance = models.IntegerField()
+    Fee_paid = models.IntegerField()
     
