c004fd840981d27e19a20a9df9d1ee02d40ea4f6
theurikarue <iambonitheuri@gmail.com>
2024-06-15 22:30:49 +0700

    ♻️  refactor:remove default value in models

--- student_register/models.py
+++ student_register/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 class Student(models.Model):
     Name = models.CharField(max_length=100,default= 0)
-    Reg_no = models.CharField(max_length=50,default= 0)
-    Mobile_no = models.IntegerField(null=False,default= 0)
-    Student_email = models.EmailField(max_length=20,default= 0)
-    Intake_year = models.DateField(auto_now=False,default= 0)
+    Reg_no = models.CharField(max_length=50)
+    Mobile_no = models.IntegerField(null=False)
+    Student_email = models.EmailField(max_length=20)
+    Intake_year = models.DateField(auto_now=False)
 
 class Hostel(models.Model):
     Student = models.ForeignKey(Student, on_delete=models.CASCADE,default = True)
